9c935f76b15f19caff06ec80a896a8264ffb2fa1
Andreas Kruger <ak@patientsky.com>
2022-01-25 12:49:36 +0100

    Fix variables when starting

--- bin/start.sh
+++ bin/start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e
 
-MODE="iptables"
-API_URL=""
-API_KEY=""
-
 echo "-> Replacing environment variables"
-
+sed -i -e "s/{MODE}/${MODE:-iptables}/g" /etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml
+sed -i -e "s/{API_URL}/${API_URL:-localhost:8080}/g" /etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml
+sed -i -e "s/{API_KEY}/${API_KEY:-none}/g" /etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml
 
 echo "-> Validating config file"
 /crowdsec-firewall-bouncer -c /etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml -t
