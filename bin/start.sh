d8e49776d70db7bf8dcd7055aba82715465349a3
Andreas Kruger <ak@patientsky.com>
2022-01-28 09:18:55 +0100

    Add option to set update freq

--- bin/start.sh
+++ bin/start.sh
@@ -4,6 +4,7 @@ set -e
 echo "-> Replacing environment variables"
 sed -i "s|{API_URL}|${API_URL:-localhost}|" /etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml
 sed -i "s|{API_KEY}|${API_KEY:-none}|" /etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml
+sed -i "s|{UPDATE_FREQ}|${UPDATE_FREQ:-10s}|" /etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml
 
 echo "-> Validating config file"
 /crowdsec-firewall-bouncer -c /etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml -t
