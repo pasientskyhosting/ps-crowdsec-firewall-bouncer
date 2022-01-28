bc11cefe25c00aedec3c41a98273d44b95be87bb
Andreas Kruger <ak@patientsky.com>
2022-01-28 09:15:45 +0100

    Update to working version

--- bin/start.sh
+++ bin/start.sh
@@ -2,9 +2,8 @@
 set -e
 
 echo "-> Replacing environment variables"
-sed -i -e "s/{MODE}/${MODE:-nftables}/g" /etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml
-sed -i -e "s/{API_URL}/${API_URL:-localhost:8080}/g" /etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml
-sed -i -e "s/{API_KEY}/${API_KEY:-none}/g" /etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml
+sed -i "s|{API_URL}|${API_URL:-localhost}|" /etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml
+sed -i "s|{API_KEY}|${API_KEY:-none}|" /etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml
 
 echo "-> Validating config file"
 /crowdsec-firewall-bouncer -c /etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml -t
